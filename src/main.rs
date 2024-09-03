4c558e2c386250331ef0ef6d320d0af0044bc6de
Kei-K23 <keiksl2301@gmail.com>
2024-09-03 15:54:46 +0630

    Refator the function to get user input file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use clap::Parser;
-use exif::{Field, Reader, Tag, Value};
+use exif::{Field, Reader, Value};
 use image::GenericImageView;
 use inquire::{InquireError, Select};
 use prettytable::{format, row, Table};
@@ -7,14 +6,8 @@ use std::{
     fs::{self, File},
     io::BufReader,
     path::Path,
+    process::exit,
 };
-
-/// Simple CLI to extract metadata from image files
-#[derive(Parser)]
-struct Args {
-    #[clap(short, long)]
-    file: String,
-}
 
 fn main() -> Result<(), InquireError> {
     let ascii_art = r#"
@@ -29,7 +22,7 @@ fn main() -> Result<(), InquireError> {
     println!("Image Processing CLI (IPC) written in Rust 🦀");
     println!();
 
-    // Step 1: Interactive selection
+    // Interactive selection for main options
     let main_options = vec!["Image processing", "Get image information data"];
     let user_choice_main = Select::new("Choose an option:", main_options.clone()).prompt()?;
 
@@ -38,7 +31,9 @@ fn main() -> Result<(), InquireError> {
     }
 
     if user_choice_main == main_options[1] {
-        // Step 1: Interactive selection
+        let (img, file) = get_image();
+
+        // Interactive selection for information options
         let info_options = vec![
             "Show Dimensions and File Size",
             "Show EXIF Data",
@@ -47,12 +42,8 @@ fn main() -> Result<(), InquireError> {
         let user_choice =
             Select::new("Choose information you want:", info_options.clone()).prompt()?;
 
-        let args: Args = Args::parse();
-        let path = Path::new(&args.file);
-
-        let img = image::open(path).expect("Could not read image");
         let dimensions = img.dimensions();
-        let file_size = fs::metadata(&args.file)
+        let file_size = fs::metadata(&file)
             .expect("Could not get file metadata")
             .len() as f64
             / 1_048_576.0; // Convert bytes to MB
@@ -61,7 +52,7 @@ fn main() -> Result<(), InquireError> {
         let mut table = Table::new();
         table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
         table.set_titles(row!["Property", "Value"]);
-        table.add_row(row!["File", args.file]);
+        table.add_row(row!["File", file]);
         table.add_row(row![
             "Dimensions",
             format!("{}x{}", dimensions.0, dimensions.1)
@@ -76,7 +67,7 @@ fn main() -> Result<(), InquireError> {
 
         if user_choice == info_options[1] || user_choice == info_options[2] {
             // Extract and display EXIF data
-            let exif_data = extract_exif_data(&args.file);
+            let exif_data = extract_exif_data(&file);
             if let Some(exif_table) = exif_data {
                 println!();
                 println!("Image EXIF data");
@@ -88,6 +79,26 @@ fn main() -> Result<(), InquireError> {
     }
 
     Ok(())
+}
+
+/// Get image file from user input
+fn get_image() -> (image::DynamicImage, String) {
+    let mut file = String::new();
+    println!("Enter image name or path: "); // Prompt stays on the same line
+    std::io::stdin()
+        .read_line(&mut file)
+        .expect("Read user input failed");
+    file = file.trim().to_string();
+
+    if !Path::new(&file).exists() {
+        println!("Could not found the file");
+        exit(1);
+    }
+    let path: &Path = Path::new(&file);
+    let image = image::open(path).expect("Could not read image");
+
+    // Return image and file name
+    (image, file)
 }
 
 /// Extracts EXIF data from the image file and returns it in a table format
